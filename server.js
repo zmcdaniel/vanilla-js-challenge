3a0bd8f9eb89fbb5710cfe36f993282f7e56af60
zmcdaniel <zmcdaniel@gmail.com>
2016-09-29 10:16:47 -0700

    Added filesync to package.json; cleaned up obvious errors with server.js; basic html on index.html

--- server.js
+++ server.js
@@ -1,32 +1,36 @@
+// Pulling in our dependencies
 var express = require('express');
-var app = express();
 var fs = require('fs');
+var bodyParser = require('body-parser');
 var path = require('path');
+var app = express();
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, 'public'));
+app.get('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/favorites', function(req, res){
+  res.send('favorites route');
   var data = fs.readFileSync('./data.json');
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
-;
+});
 
 app.get('favorites', function(req, res){
   if(!req.body.name || !req.body.oid){
     res.send("Error");
     return
-  
-  var data = JSON.parse(fs.readFileSync('./data.json'));
-  data.push(req.body);
-  fs.writeFile('./data.json', JSON.stringify(data));
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
+  } else {
+    var data = JSON.parse(fs.readFileSync('./data.json'));
+    data.push(req.body);
+    fs.writeFile('./data.json', JSON.stringify(data));
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+  }
 });
 
-app.list(3000, function(){
+app.listen(3000, function(){
   console.log("Listening on port 3000");
 });
