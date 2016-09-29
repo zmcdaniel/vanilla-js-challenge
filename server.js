c1a145192b33c956806d74830c03bda01d7a7d05
zmcdaniel <zmcdaniel@gmail.com>
2016-09-29 10:30:10 -0700

    Added request dependency

--- server.js
+++ server.js
@@ -11,18 +11,26 @@ var app = express();
 // ========
 // SET / USE STATEMENTS
 // ========
-
-//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
 
 // ========
 // ROUTES
 // ========
 app.get('/', express.static(path.join(__dirname, 'public')));
+
+
+app.post('/', function(req, res) {
+  var fileContents = fs.readFileSync('./data.json');
+  var data = JSON.parse(fileContents);
+  data.push(req.body);
+  fs.writeFileSync('./data.json', JSON.stringify(data));
+  res.redirect('/');
+});
 
 app.get('/favorites', function(req, res){
   res.send('**favorites route**');
@@ -31,18 +39,18 @@ app.get('/favorites', function(req, res){
   res.render('index', {data});
 });
 
-app.get('favorites', function(req, res){
-  if(!req.body.name || !req.body.oid){
-    res.send("Error");
-    return
-  } else {
-    var data = JSON.parse(fs.readFileSync('./data.json'));
-    data.push(req.body);
-    fs.writeFile('./data.json', JSON.stringify(data));
-    res.setHeader('Content-Type', 'application/json');
-    res.send(data);
-  }
-});
+// app.get('favorites', function(req, res){
+//   if(!req.body.name || !req.body.oid){
+//     res.send("Error");
+//     return
+//   } else {
+//     var data = JSON.parse(fs.readFileSync('./data.json'));
+//     data.push(req.body);
+//     fs.writeFile('./data.json', JSON.stringify(data));
+//     res.setHeader('Content-Type', 'application/json');
+//     res.send(data);
+//   }
+// });
 
 // ========
 // LISTEN
